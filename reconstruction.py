4856219444d0932f8df01a3c9ad46a53a13a1155
Jonathan Peck <jonathan.peck@ugent.be>
2023-10-11 14:32:25 +0200

    add shearlets

--- reconstruction.py
+++ reconstruction.py
@@ -73,7 +73,7 @@ class Reconstruction:
         """
         trial.suggest_float('undersample_rate', 0.25, 1)
         trial.suggest_categorical('subsample', ['random', 'fourier', 'dummy'])
-        trial.suggest_categorical('method', ['wavelet', 'fourier', 'dtcwt', 'dummy'])
+        trial.suggest_categorical('method', ['wavelet', 'fourier', 'dtcwt', 'shearlet', 'dummy'])
         trial.suggest_float('lam', 0, 1e4)
         trial.suggest_float('lam_decay', 0.5, 1)
 
