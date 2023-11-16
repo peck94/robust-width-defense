0db0c29dbddce1b7f3f12ea19ca096507a35c230
Jonathan Peck <jonathan.peck@ugent.be>
2023-11-16 15:04:27 +0100

    plot trials

--- plot.py
+++ plot.py
@@ -8,6 +8,8 @@ import numpy as np
 
 from sklearn.isotonic import IsotonicRegression
 
+from tqdm import tqdm
+
 if __name__ == '__main__':
     # parse arguments
     parser = argparse.ArgumentParser()
@@ -19,18 +21,27 @@ if __name__ == '__main__':
     # load study
     study = optuna.load_study(study_name=args.name, storage=args.results)
 
-    # plot Pareto front
+    # load Pareto front
     obj1, obj2 = [], []
-    for trial in study.best_trials:
+    for trial in tqdm(study.best_trials):
         obj1.append(trial.values[0])
         obj2.append(trial.values[1])
     
+    # load other trials
+    obj1s, obj2s = [], []
+    for trial in tqdm(study.trials):
+        if trial.values:
+            obj1s.append(trial.values[0])
+            obj2s.append(trial.values[1])
+    
+    # plot trials
     isoreg = IsotonicRegression(y_min=0, y_max=1, increasing=False, out_of_bounds='clip').fit(obj1, obj2)
 
     xs = np.linspace(0, 1, 100)
     ys = isoreg.predict(xs)
     
     plt.title(args.name)
+    plt.scatter(obj1s, obj2s, color='black')
     plt.scatter(obj1, obj2, color='red')
     plt.plot(xs, ys, ls='--', color='blue')
     plt.xlabel('robust accuracy')
