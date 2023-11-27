691473470a7965890b0c068c9b4a02b3d4390ad6
Jonathan Peck <jonathan.peck@ugent.be>
2023-11-27 13:17:05 +0100

    more plotting options

--- plot.py
+++ plot.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-name', type=str, default='cs-test', help='study name')
     parser.add_argument('-results', type=str, default='sqlite:///results.db', help='results database')
+    parser.add_argument('-method', type=str, default=None, help='filter method')
 
     args = parser.parse_args()
 
@@ -30,18 +31,23 @@ if __name__ == '__main__':
     obj1s, obj2s, css = [], [], []
     for trial in tqdm(study.trials):
         if trial.values:
-            obj1s.append(trial.values[0])
-            obj2s.append(trial.values[1])
-            css.append(COLORS[trial.params['method']])
+            if args.method is None or args.method == trial.params['method']:
+                obj1s.append(trial.values[0])
+                obj2s.append(trial.values[1])
+                css.append(COLORS[trial.params['method']])
     
     # plot trials
     fig, ax = plt.subplots()
     plt.title(args.name)
+    plt.plot([0, 1], [0, 1], color='black', ls=':', alpha=.5)
     plt.scatter(obj1s, obj2s, color=css, s=4)
     plt.xlabel('robust accuracy')
     plt.ylabel('standard accuracy')
     plt.xlim((0, 1))
     plt.ylim((0, 1))
 
-    ax.legend(handles=[Line2D([0], [0], color='w', markerfacecolor=COLORS[c], marker='o', label=c) for c in COLORS])
+    if args.method:
+        ax.legend(handles=[Line2D([0], [0], color='w', markerfacecolor=COLORS[args.method], marker='o', label=args.method)])
+    else:
+        ax.legend(handles=[Line2D([0], [0], color='w', markerfacecolor=COLORS[c], marker='o', label=c) for c in COLORS])
     plt.savefig(f'plots/{args.name}.pdf')
