a219950cbad9975f1f9574f6e955ebf17cbf8441
Jonathan Peck <jonathan.peck@ugent.be>
2024-02-09 16:12:32 +0100

    timeout

--- attack.py
+++ attack.py
@@ -6,6 +6,8 @@ import argparse
 
 import warnings
 
+import asyncio
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -24,26 +26,7 @@ from tabulate import tabulate
 
 from utils import Welford
 
-if __name__ == '__main__':
-    # parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-model', type=str, default='resnet50', help='model name')
-    parser.add_argument('-weights', type=str, default='IMAGENET1K_V2', help='model weights')
-    parser.add_argument('-name', type=str, default='cs-test', help='study name')
-    parser.add_argument('-results', type=str, default='sqlite:///results.db', help='results database')
-    parser.add_argument('-eps', type=int, default=4, help='perturbation budget')
-    parser.add_argument('-norm', type=str, default='Linf', help='threat model')
-    parser.add_argument('-data', type=str, default='/scratch/jpeck/imagenet', help='ImageNet path')
-    parser.add_argument('-bs', type=int, default=16, help='batch size')
-    parser.add_argument('-adapt', action='store_true', default=False, help='perform adaptive attack')
-    parser.add_argument('-trial', type=int, default=0, help='Optuna trial to load')
-    parser.add_argument('-iterations', type=int, default=10, help='number of iterations of smoothing')
-    parser.add_argument('-rb', action='store_true', default=False, help='use RobustBench models')
-    parser.add_argument('-attack', choices=['autoattack', 'simba'], help='adversarial attack to run')
-    parser.add_argument('-softmax', action='store_true', default=False, help='predict softmax probabilities')
-
-    args = parser.parse_args()
-
+async def main(args):
     # perform checks
     if args.adapt and args.attack == 'simba' and not args.softmax:
         warnings.warn('This attack expects probabilities. Consider passing the -softmax flag.', RuntimeWarning)
@@ -87,10 +70,11 @@ if __name__ == '__main__':
     orig_acc = Welford()
     adv_acc = Welford()
     progbar = tqdm(data_loader)
+    loop = asyncio.get_event_loop()
     for x_batch, y_batch in progbar:
         try:
-            x_adv = adversary.generate(x_batch.detach(), y_batch.detach())
-
+            x_adv = await asyncio.wait_for(loop.run_in_executor(None, lambda: adversary.generate(x_batch.detach(), y_batch.detach())), timeout=args.timeout)
+            
             with torch.no_grad():
                 y_pred_orig = defense(x_batch.detach().to(device)).cpu().detach().numpy()
                 y_pred = defense(x_adv.detach().to(device)).cpu().detach().numpy()
@@ -99,6 +83,8 @@ if __name__ == '__main__':
             adv_acc.update_all(y_pred.argmax(axis=1) == y_batch.numpy())
 
             progbar.set_postfix({'orig_acc': orig_acc.values[0], 'adv_rec_acc': adv_acc.values[0]})
+        except asyncio.TimeoutError:
+            print('Timed out.')
         except RuntimeError as e:
             print(e)
 
@@ -114,3 +100,26 @@ if __name__ == '__main__':
         ['Standard', f'{orig_mean:.2%}', f'{orig_err:.2%}'],
         ['Robust', f'{adv_mean:.2%}', f'{adv_err:.2%}']
     ], headers=['Setting', 'Accuracy', 'Error']))
+
+if __name__ == '__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-model', type=str, default='resnet50', help='model name')
+    parser.add_argument('-weights', type=str, default='IMAGENET1K_V2', help='model weights')
+    parser.add_argument('-name', type=str, default='cs-test', help='study name')
+    parser.add_argument('-results', type=str, default='sqlite:///results.db', help='results database')
+    parser.add_argument('-eps', type=int, default=4, help='perturbation budget')
+    parser.add_argument('-norm', type=str, default='Linf', help='threat model')
+    parser.add_argument('-data', type=str, default='/scratch/jpeck/imagenet', help='ImageNet path')
+    parser.add_argument('-bs', type=int, default=16, help='batch size')
+    parser.add_argument('-adapt', action='store_true', default=False, help='perform adaptive attack')
+    parser.add_argument('-trial', type=int, default=0, help='Optuna trial to load')
+    parser.add_argument('-iterations', type=int, default=10, help='number of iterations of smoothing')
+    parser.add_argument('-rb', action='store_true', default=False, help='use RobustBench models')
+    parser.add_argument('-attack', choices=['autoattack', 'simba'], help='adversarial attack to run')
+    parser.add_argument('-softmax', action='store_true', default=False, help='predict softmax probabilities')
+    parser.add_argument('-timeout', type=int, default=60, help='per-batch timeout for attacks (in seconds)')
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args))
