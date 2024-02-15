a2388334c8c86770389be78a0c87ef69c9b0c496
Jonathan Peck <jonathan.peck@ugent.be>
2024-02-15 15:39:25 +0100

    cleanup

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # ResNet-50
-python attack.py -model resnet50 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 32 -adapt -trial 273 -iterations 1 -attack autoattack -timeout 120
+python attack.py -log logs/resnet50_simba.json -model resnet50 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 1 -adapt -trial 273 -iterations 1 -attack simba -timeout 120 -softmax
