9f16380d79646f49b4a07b9edbf2d96e2f040758
Jonathan Peck <jonathan.peck@ugent.be>
2024-03-12 17:51:55 +0100

    ordering

--- experiments.sh
+++ experiments.sh
@@ -10,9 +10,6 @@ python attack.py -log logs/resnet50.json -model resnet50 -weights IMAGENET1K_V2 
 # Wide-Resnet101
 python attack.py -log logs/wide_resnet101.json -model wide_resnet101_2 -results sqlite:///fourier.db -name wide_resnet101 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 106 -iterations 1 -attack autoattack
 
-# Wong2020Fast
-python attack.py -log logs/wong2020fast.json -model Wong2020Fast -rb -base -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 273 -iterations 1 -attack autoattack
-
 # ViT-B-16
 python attack.py -log logs/vit_b_16.json -model vit_b_16 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name vit_b_16 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 52 -iterations 1 -attack autoattack
 
@@ -22,6 +19,9 @@ python attack.py -log logs/swin_t.json -model swin_t -weights IMAGENET1K_V2 -res
 #################
 # Robust models #
 #################
+
+# Wong2020Fast
+python attack.py -log logs/wong2020fast.json -model Wong2020Fast -rb -base -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 273 -iterations 1 -attack autoattack
 
 # Peng2023Robust
 python attack.py -log logs/peng2023robust.json -model Peng2023Robust -rb -base -results sqlite:///fourier.db -name wide_resnet101 -eps 4 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 106 -iterations 1 -attack autoattack
