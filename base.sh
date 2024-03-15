ac7f68a13facd4dbb70c53c067ed434c75a9d300
Jonathan Peck <jonathan.peck@ugent.be>
2024-03-15 13:54:03 +0100

    fix

--- base.sh
+++ base.sh
@@ -10,9 +10,6 @@
 # Wide-Resnet101
 #python attack.py -log logs/wide_resnet101_base.json -model wide_resnet101_2 -results sqlite:///fourier.db -name wide_resnet101 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -base -trial 106 -iterations 1 -attack autoattack
 
-# Wong2020Fast
-#python attack.py -log logs/wong2020fast_base.json -model Wong2020Fast -rb -base -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -base -trial 273 -iterations 1 -attack autoattack
-
 # ViT-B-16
 python attack.py -weights IMAGENET1K_V1 -log logs/vit_b_16_base.json -model vit_b_16 -results sqlite:///fourier.db -name vit_b_16 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -base -trial 52 -iterations 1 -attack autoattack
 
