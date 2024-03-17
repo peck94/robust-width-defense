f0e6b75417e3b99e885fbc86f4e8e416bcf3bc75
Jonathan Peck <jonathan.peck@ugent.be>
2024-03-17 18:05:33 +0100

    experiments

--- experiments.sh
+++ experiments.sh
@@ -38,15 +38,19 @@ python attack.py -log logs/robust/liu2023comprehensive_swin-l.json -model Liu202
 
 # ResNet-50
 python attack.py -log logs/cs/resnet50.json -model resnet50 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 273 -iterations 1 -attack autoattack
+python attack.py -log logs/xfer/resnet50.json -model resnet50 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name resnet50 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -trial 273 -iterations 1 -attack autoattack
 
 # Wide-Resnet101
 python attack.py -log logs/cs/wide_resnet101.json -model wide_resnet101_2 -results sqlite:///fourier.db -name wide_resnet101 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 106 -iterations 1 -attack autoattack
+python attack.py -log logs/xfer/wide_resnet101.json -model wide_resnet101_2 -results sqlite:///fourier.db -name wide_resnet101 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -trial 106 -iterations 1 -attack autoattack
 
 # ViT-B-16
 python attack.py -log logs/cs/vit_b_16.json -model vit_b_16 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name vit_b_16 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 52 -iterations 1 -attack autoattack
+python attack.py -log logs/xfer/vit_b_16.json -model vit_b_16 -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name vit_b_16 -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -trial 52 -iterations 1 -attack autoattack
 
 # Swin-T
 python attack.py -log logs/cs/swin_t.json -model swin_t -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name swin_t -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 275 -iterations 1 -attack autoattack
+python attack.py -log logs/xfer/swin_t.json -model swin_t -weights IMAGENET1K_V2 -results sqlite:///fourier.db -name swin_t -eps 16 -norm Linf -data "$VSC_DATA_VO/ImageNet" -bs 4 -adapt -trial 275 -iterations 1 -attack autoattack
 
 #################
 # RB+CS models  #
